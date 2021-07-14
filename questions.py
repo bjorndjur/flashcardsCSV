32e052ee7c71fedcfc1ff8c0c6376875a27676d8
Oskar Mathsson <oskar.mathsson@gmail.com>
2021-07-14 20:29:01 +0200

    Result added

--- questions.py
+++ questions.py
@@ -9,6 +9,14 @@ class Question:
         self.answer = answer
 
 
+class Answers:
+    def __init__(self, question, correct, selected, isCorrect):
+        self.question = question
+        self.correct = correct
+        self.selected = selected
+        self.isCorrect = isCorrect
+
+
 
 def loadQuestions(file):
     questions = []
@@ -32,8 +40,9 @@ def getNextQuestion(questions, questionCounter):
 # Get four respons options
 def getResponsOptions(questions, questionCounter):
     responsOptions = []
+    randomSample = []
     responsOptions.append(questions[questionCounter])
-    questions.pop(questionCounter)
+    #questions.pop(questionCounter)
     randomSample = random.sample(range(0, len(questions)), 3)
 
     for i in range(len(randomSample)):
@@ -43,33 +52,58 @@ def getResponsOptions(questions, questionCounter):
 
 
 def trySelectedAnswer(currentQuestion,responsOptions):
+
+
     if (currentQuestion.answer == responsOptions.answer):
-        return True
+        return Answers(currentQuestion.question, currentQuestion.answer, responsOptions.answer, True)
     else:
-        return False
+        return Answers(currentQuestion.question, currentQuestion.answer, responsOptions.answer, False)
+
+
+
+def result(answers):
+    counterCorrect = 0
+    for i in range(len(answers)):
+        print(answers[i].question)
+        if (answers[i].isCorrect == True):
+            counterCorrect += 1
+            print('Your answer: {correct}\n'.format(correct=answers[i].correct))
+        else:
+            print('Your answer: {selected}'.format(selected = answers[i].selected))
+            print('Correct anser: [{correct}]\n'.format(correct=answers[i].correct))
+
+    print('{numberOfCorrect}/{numberOfQuestions}'.format(numberOfCorrect=counterCorrect, numberOfQuestions=len(answers)))
+
 
 
 
 questionCounter = 0
+answers = []
+
 
 # Read questions and answers 
 questions = loadQuestions('subjects/first.csv')
 
-# Get current question
-currentQuestion = getNextQuestion(questions, questionCounter)
-print(currentQuestion.question)
-print(currentQuestion.answer)
 
-# Get respons options
-responsOptions = getResponsOptions(questions,questionCounter)
-random.shuffle(responsOptions)
-for i in range(len(responsOptions)):
-    print('{i}: {responsOption}'.format(i=i+1, responsOption=responsOptions[i].answer))
+while (questionCounter < len(questions)):
+    # Get current question
+    currentQuestion = getNextQuestion(questions, questionCounter)
+    print(questionCounter + 1, currentQuestion.question)
+    # print(currentQuestion.answer)
+
+    # Get respons options
+    responsOptions = getResponsOptions(questions,questionCounter)
+    random.shuffle(responsOptions)
+    for i in range(len(responsOptions)):
+        print('{i}: {responsOption}'.format(i=i+1, responsOption=responsOptions[i].answer))
+
+    # Selected answer
+    selectedAnswer = input('>')
+
+    answer = trySelectedAnswer(currentQuestion,responsOptions[int(selectedAnswer) - 1])
+    answers.append(answer)
+    #print(answer.isCorrect)
+    questionCounter += 1
 
 
-# Selected answer
-selectedAnswer = input('>')
-
-isCorrect = trySelectedAnswer(currentQuestion,responsOptions[int(selectedAnswer) - 1])
-
-print(isCorrect)
+result(answers)
